b887ac62167c2ad7c13b1f01e7d415d86bbe4034
guohao <guohao.qa@bytedance.com>
2018-03-16 19:38:55 +0800

    update

--- TBUIAutoTest/UIImage+TBUIAutoTest.m
+++ TBUIAutoTest/UIImage+TBUIAutoTest.m
@@ -24,7 +24,7 @@
 //    // 在自动化测试时无法通过脚本来打开这个选项，所以只能在真机下的debug版默认打开
 //    isAutoTestUI = YES;
 //#endif
-    isAutoTestUI = YES;
+    BOOL isAutoTestUI = YES;
     if (isAutoTestUI)
     {
         static dispatch_once_t onceToken;
